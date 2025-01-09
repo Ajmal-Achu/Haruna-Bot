5c58c21e526fca54a532c162d1c8e4a6322e976f
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2025-01-09 19:49:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const Config = {
-	phone_number: "6289512149646", // Number BOT for pairing
-	owners: ["6281282405626"], // Number Owner for specific feature access
+	phone_number: "919744196187", // Number BOT for pairing
+	owners: ["918590508376"], // Number Owner for specific feature access
 
 	use_pairing_code: true,
 	pairing_wait: 1000 * 6,
